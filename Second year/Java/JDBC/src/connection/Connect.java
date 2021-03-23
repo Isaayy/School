fe52e29b3a6e691f2d66e85fac349f89c79662c7
Isaayy <kuba.serwin99@gmail.com>
2021-03-23 19:21:45 +0100

    Fetch tables

--- Second year/Java/JDBC/src/connection/Connect.java
+++ Second year/Java/JDBC/src/connection/Connect.java
@@ -1,37 +1,73 @@
 package connection;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Connect {
 
     private String driver = "org.postgresql.Driver";
 
-    private String host = "****";
+    private String host;
+    private String port;
+    private String dbname;
+    private String user;
 
-    private String port = "****";
+    private String url ;
+    private String pass;
 
-    private String dbname = "****";
-    private String user = "****";
+    private List<String> tables = new ArrayList();
 
-    private String url = "jdbc:postgresql://" + host+":"+port + "/" + dbname;
-    private String pass = "****";
+
+    public void readFile() {
+        try {
+            File myObj = new File("C:\\Users\\Jakub\\Desktop\\data.txt");
+            Scanner myReader = new Scanner(myObj);
+            List<String> data = new ArrayList();
+
+            while (myReader.hasNextLine()) {
+                data.add(myReader.nextLine()) ;
+            }
+            myReader.close();
+
+            this.host = data.get(0);
+            this.port = data.get(1);
+            this.dbname = data.get(2);
+            this.user = data.get(3);
+            this.pass = data.get(4);
+            this.url =  "jdbc:postgresql://" + this.host+":"+this.port + "/" + this.dbname;
+
+
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
     private Connection connection;
     private Statement statement;
 
-    public Connect () {
-        connection = makeConnection(); }
+    public Connect () {}
 
     public Connection getConnection(){
+        if(this.url == null) return null;
+        connection = makeConnection();
+
         return(connection);
     }
+
     public void close() {
         try {
-
             connection.close(); }
 
         catch (SQLException sqle){
             System.err.println("Blad przy zamykaniu polaczenia: " + sqle);
 
-        } }
+        }
+    }
 
     public Connection makeConnection(){
         try {
@@ -39,20 +75,11 @@ public class Connect {
             Connection connection = DriverManager.getConnection(url, user, pass);
             System.out.println("Pomyslnie nawiazano polaczenie ");
 
-//            statement = connection.createStatement();
-//            ResultSet rs = statement.executeQuery( "select * from dziekanat.\"adresy\" ;" );
-//
-//            while ( rs.next() ) {
-//
-//                int idAdresu = rs.getInt("id_adresu");
-//
-//                String  ulica = rs.getString("ulica");
-//
-//                System.out.println(idAdresu);
-//                System.out.println(ulica);
-//
-//            }
-
+            DatabaseMetaData md = connection.getMetaData();
+            ResultSet rs = md.getTables(null, null, "%", null);
+            while (rs.next()) {
+                this.tables.add(rs.getString(3));
+            }
             return(connection);
 
         }
@@ -66,4 +93,9 @@ public class Connect {
 
             return(null);
         }
-    } }
+    }
+
+    public List<String> getTables() {
+        return this.tables;
+    }
+}
--- Second year/Java/JDBC/src/pl/edu/pwsztar/Main.java
+++ Second year/Java/JDBC/src/pl/edu/pwsztar/Main.java
@@ -2,11 +2,16 @@ package pl.edu.pwsztar;
 
 import connection.Connect;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 	    Connect connect = new Connect();
+	    connect.readFile();
         connect.getConnection();
 
+        List<String> tables = connect.getTables();
+        tables.forEach(System.out::println);
     }
 }
