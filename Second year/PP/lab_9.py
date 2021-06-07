56be813aef1854e3e31076506880ac56861a74c5
Isaayy <kuba.serwin99@gmail.com>
2021-06-07 13:07:11 +0200

    Lab_9

--- Second year/PP/lab_9.py
+++ Second year/PP/lab_9.py
@@ -10,6 +10,12 @@ def sum_list2(list,length = 0,sum=0):
         return sum
         
     return sum_list2(list,length-1,sum+list[length-1])
+    
+def f_variation(arr, arr_len, s=0):
+    if len(arr) > 0:
+        variation = f_variation(arr[1:], arr_len, s + arr[0])
+        return variation[0], variation[1] + (arr[0] - variation[0])**2 / (arr_len - 1)
+    return s/arr_len, 0
 
 
 if __name__ == "__main__":
