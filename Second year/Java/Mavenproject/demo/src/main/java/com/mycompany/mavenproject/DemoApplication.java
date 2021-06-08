f35fb56e7e9f9276fb7b33816f53a7ef6b57b237
Isaayy <kuba.serwin99@gmail.com>
2021-06-08 09:10:50 +0200

    Task 7 final

--- Second year/Java/Mavenproject/demo/src/main/java/com/mycompany/mavenproject/DemoApplication.java
+++ Second year/Java/Mavenproject/demo/src/main/java/com/mycompany/mavenproject/DemoApplication.java
@@ -4,16 +4,30 @@ import spring.jdbc.impl.AccountServiceImp;
 import spring.model.Account;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import spring.service.AccountService;
 
 import java.util.List;
 
 public class DemoApplication {
 	public static void main(String[] args) throws Exception {
 
+		// Zad 6 - zrodlo danych - plik csv
+
+//		ApplicationContext appCtx =
+//				new ClassPathXmlApplicationContext("applicationContext.xml");
+//		AccountService accountService =
+//				(AccountService)appCtx.getBean("accountService");
+//
+//		List<Account> delinquentAccounts = accountService.findDeliquentAccounts();
+
+
+		// Zad 7 - zrodlo danych - baza danych
+
 		ApplicationContext appCtx =
 				new ClassPathXmlApplicationContext("applicationContext.xml");
 
 		AccountServiceImp accountServiceImp = (AccountServiceImp)appCtx.getBean("AccountServiceImp");
+
 
 		List<Account> delinquentAccounts = accountServiceImp.findAll();
 
